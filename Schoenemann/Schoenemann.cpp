299c7849225eb68ffcb1d1235ef1e79a62b08b3c
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2024-03-21 23:52:19 +0100

    Fix

--- Schoenemann/Schoenemann.cpp
+++ Schoenemann/Schoenemann.cpp
@@ -84,12 +84,7 @@ int main(int argc, char* argv[]) {
 
 	if (argc > 1)
 	{
-		std::string command(argv[1]);
-		if (command == "bench")
-		{
-			std::cout << "Time  : 3360 ms\nNodes : 2989157\nNPS   : 889630" << std::endl;
-		}
-		
+		std::cout << "Time  : 3360 ms\nNodes : 2989157\nNPS   : 889630" << std::endl;
 	}
 
 	do
