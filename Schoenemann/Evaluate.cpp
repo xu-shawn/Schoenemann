7582f8d9d2400dfdd083aebfa3a9065e89f9c8e8
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2024-03-27 22:22:18 +0100

    Evaluation fix

--- Schoenemann/Evaluate.cpp
+++ Schoenemann/Evaluate.cpp
@@ -9,22 +9,28 @@ const short rookValue = 500;
 const short queenValue = 900;
 
 int evaluate(chess::Board& board) {
+    int evaluation = 0;
+    int white_evaluation = count_material(board, chess::Color::WHITE);
+    int black_evaluation = count_material(board, chess::Color::BLACK);
+
+    evaluation = white_evaluation - black_evaluation;
+
+    int perspective = board.sideToMove() == chess::Color::WHITE ? 1 : -1;
+
+    return evaluation * perspective;
+}
+
+int count_material(chess::Board& board, chess::Color color) {
     int material = 0;
-    material += count_amount(board, chess::PieceType::PAWN) * pawnValue;
-    material += count_amount(board, chess::PieceType::KNIGHT) * knightValue;
-    material += count_amount(board, chess::PieceType::BISHOP) * bishopValue;
-    material += count_amount(board, chess::PieceType::ROOK) * rookValue;
-    material += count_amount(board, chess::PieceType::QUEEN) * queenValue;
-
-    if (board.sideToMove() == chess::Color::BLACK)
-    {
-        material = -material;
-    }
-
+    material += count_amount(board, chess::PieceType::PAWN, color) * pawnValue;
+    material += count_amount(board, chess::PieceType::KNIGHT, color) * knightValue;
+    material += count_amount(board, chess::PieceType::BISHOP, color) * bishopValue;
+    material += count_amount(board, chess::PieceType::ROOK, color) * rookValue;
+    material += count_amount(board, chess::PieceType::QUEEN, color) * queenValue;
     return material;
 }
 
-int count_amount(chess::Board& board, chess::PieceType type) {
-    chess::Bitboard bitboard = board.pieces(type, board.sideToMove());
+int count_amount(chess::Board& board, chess::PieceType type, chess::Color color) {
+    chess::Bitboard bitboard = board.pieces(type, color);
     return bitboard.count();
 }
--- Schoenemann/Evaluate.h
+++ Schoenemann/Evaluate.h
@@ -3,4 +3,5 @@
 #include "movegen/chess.hpp"
 
 int evaluate(chess::Board& board);
-int count_amount(chess::Board& board, chess::PieceType type);
+int count_material(chess::Board& board, chess::Color color);
+int count_amount(chess::Board& board, chess::PieceType type, chess::Color color);
