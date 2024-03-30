a79eb674935e2a14c63c1adc2871068203470a44
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2024-03-30 01:25:53 +0100

    Cleanup

--- Schoenemann/Search.h
+++ Schoenemann/Search.h
@@ -8,4 +8,3 @@ int search(int depth, int alpha, int beta, int ply, Board& board);
 chess::Move& getBestMove();
 int quiescence_search(int alpha, int beta, Board& board);
 void iterative_deepening(Board& board);
-void time_keeper(int time_for_move);
