b9917f663a7a99eeb2f248a8e4a4e5ba1b443915
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2024-05-07 23:15:18 +0200

    Changes

--- Schoenemann/psqt.cpp
+++ Schoenemann/psqt.cpp
@@ -6,81 +6,43 @@
 int psqt::getPieceBounus(Board& board, PieceType piece, Color& color)
 {
 	int bounus = 0;
-	bool isBlack = board.sideToMove() == Color::WHITE ? true : false;
 	Bitboard pieces = board.pieces(piece, color);
-	std::vector<int> indexs = getSetBitsEfficient(pieces);
+	bool isBlacksTurn = board.sideToMove() == Color::BLACK ? false : true;
+	std::vector<int> indexs = getSetBitsEfficient(pieces, isBlacksTurn);
 
 	if (piece == PieceType::PAWN)
 	{
 		for (int index : indexs)
 		{
-			for (int index : indexs)
-			{
-				if (isBlack)
-				{
-					bounus += blackPawnTable[index];
-				}
-				else
-				{
-					bounus += pawnTable[index];
-				}
-			}
+			bounus += pawnTable[index];
 		}
 	}
 	else if (piece == PieceType::KNIGHT)
 	{
 		for (int index : indexs)
 		{
-			if (isBlack)
-			{
-				bounus += blackKnightTable[index];
-			}
-			else
-			{
-				bounus += knightTable[index];
-			}
+			bounus += knightTable[index];
 		}
 	}
 	else if (piece == PieceType::BISHOP)
 	{
 		for (int index : indexs)
 		{
-			if (isBlack)
-			{
-				bounus += blackBishopTable[index];
-			}
-			else
-			{
-				bounus += bishopTable[index];
-			}
+			bounus += bishopTable[index];
 		}
 	}
 	else if (piece == PieceType::ROOK)
 	{
 		for (int index : indexs)
 		{
-			if (isBlack)
-			{
-				bounus += blackRookTable[index];
-			}
-			else
-			{
-				bounus += rookTable[index];
-			}
+			bounus += rookTable[index];
 		}
 	}
 	else if (piece == PieceType::QUEEN)
 	{
 		for (int index : indexs)
 		{
-			if (isBlack)
-			{
-				bounus += blackQueenTable[index];
-			}
-			else
-			{
-				bounus += queenTable[index];
-			}
+			bounus += queenTable[index];
 		}
 	}
 	else if (piece == PieceType::KING)
@@ -99,23 +61,11 @@ int psqt::getPieceBounus(Board& board, PieceType piece, Color& color)
 	return bounus;
 }
 
-Bitboard psqt::reverse(Bitboard& bitboard)
-{
-	std::uint64_t r = bitboard.getBits();
-	r = ((r >> 1) & 0x5555555555555555) | ((r & 0x5555555555555555) << 1);
-	r = ((r >> 2) & 0x3333333333333333) | ((r & 0x3333333333333333) << 2);
-	r = ((r >> 4) & 0x0F0F0F0F0F0F0F0F) | ((r & 0x0F0F0F0F0F0F0F0F) << 4);
-	r = ((r >> 8) & 0x00FF00FF00FF00FF) | ((r & 0x00FF00FF00FF00FF) << 8);
-	r = ((r >> 16) & 0x0000FFFF0000FFFF) | ((r & 0x0000FFFF0000FFFF) << 16);
-	r = (r >> 32) | (r << 32);
-	return Bitboard(r);
-}
-
-std::vector<int> psqt::getSetBitsEfficient(Bitboard& bitboard) {
+std::vector<int> psqt::getSetBitsEfficient(Bitboard& bitboard, bool isBlackTurn) {
 	std::vector<int> setBits;
 	for (int i = 0; i < 64; ++i) {
 		if ((bitboard.getBits() >> i) & 1ULL) {
-			setBits.push_back(i);
+			isBlackTurn ? setBits.push_back(63 - i) : setBits.push_back(i);
 		}
 	}
 	return setBits;
