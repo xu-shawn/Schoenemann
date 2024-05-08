583eb63e739966faa937f228aa6520f37db20d89
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2024-05-09 00:31:57 +0200

    Update Evaluate.cpp

--- Schoenemann/Evaluate.cpp
+++ Schoenemann/Evaluate.cpp
@@ -37,9 +37,15 @@ int count_material(Board& board, Color color) {
     int material = 0;
     psqt bouns;
     //Pawn
-    material += count_amount(board, PieceType::PAWN, color) * (pawnValue + bouns.getPieceBounus(board, PieceType::PAWN, color));
+    short amountPawn = count_amount(board, PieceType::PAWN, color);
+    int valuePawn = pawnValue + bouns.getPieceBounus(board, PieceType::PAWN, color);
+    material += amountPawn * valuePawn;
     //std::cout << "color " << color << " pawnvalue " << count_amount(board, PieceType::PAWN, color) * (pawnValue + bouns.getPieceBounus(board, PieceType::PAWN, color)) << " amount " << count_amount(board, PieceType::PAWN, color) << " bounus " << bouns.getPieceBounus(board, PieceType::PAWN, color) << std::endl;
-    material += count_amount(board, PieceType::KNIGHT, color) * (knightValue + bouns.getPieceBounus(board, PieceType::KNIGHT, color));
+    
+    short amountKnight = count_amount(board, PieceType::KNIGHT, color);
+    int valueKnight = knightValue + bouns.getPieceBounus(board, PieceType::KNIGHT, color);
+    material += amountKnight * valueKnight;
+
 
     short bishopAmount = count_amount(board, PieceType::BISHOP, color);
 
@@ -48,10 +54,19 @@ int count_material(Board& board, Color color) {
         material += 15;
     }
 
-    material += bishopAmount * (bishopValue + bouns.getPieceBounus(board, PieceType::BISHOP, color));
+    int valueBishop = bishopValue + bouns.getPieceBounus(board, PieceType::BISHOP, color);
 
-    material += count_amount(board, PieceType::ROOK, color) * (rookValue + bouns.getPieceBounus(board, PieceType::ROOK, color));
-    material += count_amount(board, PieceType::QUEEN, color) * (queenValue + bouns.getPieceBounus(board, PieceType::QUEEN, color));
+    material += bishopAmount * valueBishop;
+
+    short amountRook = count_amount(board, PieceType::ROOK, color);
+    int valueRook = rookValue + bouns.getPieceBounus(board, PieceType::ROOK, color);
+
+    material += amountRook * valueRook;
+
+    short amountQueen = count_amount(board, PieceType::QUEEN, color);
+    int valueQueen = queenValue + bouns.getPieceBounus(board, PieceType::QUEEN, color);
+
+    material += amountQueen * valueQueen;
     //material += bouns.getPieceBounus(board, PieceType::KING, color);
     return material;
 }
