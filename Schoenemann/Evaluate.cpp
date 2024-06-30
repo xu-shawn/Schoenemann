fd5044a7547c311145a57eb723cc456034544833
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2024-06-30 17:19:25 +0200

    Update Evaluate.cpp

--- Schoenemann/Evaluate.cpp
+++ Schoenemann/Evaluate.cpp
@@ -70,7 +70,7 @@ int count_material(Board& board, Color color) {
 
     material += amountQueen * valueQueen;
     
-    material += bouns.getPieceBounus(board, PieceType::KING, color);
+    //material += bouns.getPieceBounus(board, PieceType::KING, color);
     return material;
 }
 
