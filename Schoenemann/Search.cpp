3b0829a374ea0ec15f7da958492f12c238087215
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2024-08-19 11:55:16 +0200

    Bench 4874436

--- Schoenemann/Search.cpp
+++ Schoenemann/Search.cpp
@@ -323,42 +323,53 @@ bool Search::see(Board& board, Color color, Move move, int cutoff)
     //std::cout << "Lets go gammbling" << std::endl;
     static constexpr int SEE_PIECE_VALS[6] = {100, 400, 400, 600, 1150, 0};
 
-    if(move.typeOf() == Move::CASTLING)
+    // Checks if the move type is either casteling or enPassant
+    // If this is the case we can return from the SEE
+    if(move.typeOf() == Move::CASTLING || move.typeOf() == Move::ENPASSANT)
     {
         return 0 >= cutoff;
     }
+
     Square toSquare = move.to();
+
+    // Get the value of the captured piece
     int capturedPiece = (board.at(toSquare).type() == PieceType::NONE) ? 0 : SEE_PIECE_VALS[board.at(toSquare).type()];
 
+    // We immediately return when capturing a free piece is not enough
     int val = capturedPiece - cutoff;
     if (val < 0)
     {
         return false;
     }
 
+    // Flip the color
     int seeColor = color ^ 1;
     PieceType lastPiece = PieceType::NONE;
 
     Bitboard occ = (board.occ() ^ move.from().index()) | toSquare.index();
-    Bitboard attackers = getAttackes(toSquare, occ, board, color) & move.from().index();
+    Bitboard attackers = getAttackes(toSquare, occ, board, color) & ~move.from().index();
 
     while (true)
     {
-        PieceType smallestAttacker = getLeastValuableAttacker(board, toSquare, occ);   
+        PieceType smallestAttacker = getLeastValuableAttacker(board, toSquare, occ, seeColor);   
         if (smallestAttacker == PieceType::NONE)
         {
             break;
         }
 
+        // We flip the color again
         seeColor ^= 1;
+
+        // We remove the used attacker from the bitboard with the bitwise XOR
         attackers ^= (int)smallestAttacker;
         occ ^= (int)smallestAttacker;
 
+        // We also check for possibl X-Rays and remove them with bitwise OR
         attackers |= getXRayPieceMap(Color::WHITE, toSquare, occ, board) | getXRayPieceMap(Color::BLACK, toSquare, occ, board);
 
         val = -val - 1 - SEE_PIECE_VALS[lastPiece];
-        if (val >= (int) PieceType::NONE)
-        {
+        if (val >= 0)
+        {   // If it is the case where the last piece is the king to recapture we flip the color once again
             if (lastPiece == PieceType::KING && (attackers & board.us(seeColor)))
             {
                 seeColor ^= 1;
@@ -366,8 +377,6 @@ bool Search::see(Board& board, Color color, Move move, int cutoff)
             break;
         }
     }
-
-    
     return ((int)color != seeColor);
 }   
 
@@ -382,9 +391,8 @@ Bitboard Search::getXRayPieceMap(Color color, Square square, Bitboard occ, Board
     return (xRay & occ);
 }
 
-PieceType Search::getLeastValuableAttacker(Board& board, Square square, Bitboard occ)
+PieceType Search::getLeastValuableAttacker(Board& board, Square square, Bitboard occ, Color color)
 {
-    Color color = board.sideToMove();
     if (attacks::pawn(~color, square) & board.pieces(PieceType::PAWN, color))
     {
         return PieceType::PAWN;
