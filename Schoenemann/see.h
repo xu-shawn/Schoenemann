2433904dfc305ee55bb6a4c04084d3ee0e9c8f6e
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2024-08-23 10:44:53 +0200

    Bench 780161

--- Schoenemann/see.h
+++ Schoenemann/see.h
@@ -1,7 +1,9 @@
 #include "chess.hpp"
 
 using namespace chess;
+
 const int SEE_PIECE_VALUES[7] = {100, 300, 300, 500, 900, 0, 0};
+
 int getPieceValue(const Board &board, Move &move);
 bool see(const Board &board, Move &move, int cutoff);
 PieceType getLeastValuableAttacker(const Board &board, Bitboard &occ, Bitboard attackers, Color color);
