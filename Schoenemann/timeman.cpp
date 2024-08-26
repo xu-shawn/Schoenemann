05081b52a1bd0b9c08831e8d7f165a02e3c6a088
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2024-08-26 09:45:47 +0200

    Bench 280881

--- Schoenemann/timeman.cpp
+++ Schoenemann/timeman.cpp
@@ -6,6 +6,11 @@ int getTimeForMove()
 	int increment = getIncrement();
 	int timeForThisMove = timeLeft / 20 + (increment / 2);
 
+	if (timeForThisMove >= timeLeft)
+	{
+		timeForThisMove = timeLeft - 300;
+	}
+
 	if (timeForThisMove < 0)
 	{
 		timeForThisMove = -20;
