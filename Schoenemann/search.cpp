da3b3371ef5100836ca05fbd2dde73eab2efd17c
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2024-08-29 01:23:38 +0200

    Bench 253826

--- Schoenemann/search.cpp
+++ Schoenemann/search.cpp
@@ -325,7 +325,7 @@ int Search::qs(int alpha, int beta, Board& board, int ply)
     for (Move& move : moveList)
     {
         //Fultiy Prunning
-        if (!see(board, move, 1) && standPat + 200 <= alpha)
+        if (!see(board, move, 1) && standPat + SEE_PIECE_VALUES[board.at(move.to()).type()] <= alpha)
         {
             continue;
         }
