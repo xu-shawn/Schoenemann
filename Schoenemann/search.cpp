b4d0904e40c575da4c7274ea2782961081fc66f2
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2024-08-29 14:16:08 +0200

    Bench 275325

--- Schoenemann/search.cpp
+++ Schoenemann/search.cpp
@@ -169,6 +169,10 @@ int Search::pvs(int alpha, int beta, int depth, int ply, Board& board)
             return 0;
         }
     }
+    else if(board.isHalfMoveDraw() || board.isRepetition() || board.isInsufficientMaterial())
+    {
+        return 0;
+    }
 
     int scoreMoves[218] = {0};
     //Sort the list
