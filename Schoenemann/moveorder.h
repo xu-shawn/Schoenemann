17017215acbb4428bee6d2c03b1836cbf671f90d
Shawn Xu <xu107288696@gmail.com>
2024-09-29 20:56:50 -0700

    Fix Move Sorting
    Results of sn-new vs sn-old (8+0.08, NULL, 64MB, Pohl.epd):
    Elo: 71.78 +/- 21.94, nElo: 98.78 +/- 29.30
    LOS: 100.00 %, DrawRatio: 38.52 %, PairsRatio: 2.86
    Games: 540, Wins: 232, Losses: 122, Draws: 186, Points: 325.0 (60.19 %)
    Ptnml(0-2): [11, 32, 104, 82, 41], WL/DD Ratio: 1.89
    LLR: 2.95 (-2.94, 2.94) [0.00, 8.00]
    
    bench 9639446

--- Schoenemann/moveorder.h
+++ Schoenemann/moveorder.h
@@ -10,7 +10,7 @@
 
 using namespace chess;
 
-void orderMoves(Movelist moveList, Hash* entry, Board& board, int scores[]);
-Move sortByScore(Movelist moveList, int scores[], int i);
+void orderMoves(Movelist& moveList, Hash* entry, Board& board, int scores[]);
+Move sortByScore(Movelist& moveList, int scores[], int i);
 const int hashMoveScore = 2'147'483'647, promotion = 1'000'000'000, goodCapture = 1'500'000'000, badCapture = -500'000'000;
 const int PIECE_VALUES[7] = {100, 302, 320, 500, 900, 15000, 0};
